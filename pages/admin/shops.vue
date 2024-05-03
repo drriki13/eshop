7408f576dddc8c863f386e4e4eddfed17a96ca44
Александр Бессонов <aleksandrbessonov667@gmail.com>
2024-05-03 19:39:19 +0300

    fetch

--- pages/admin/shops.vue
+++ pages/admin/shops.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="flex flex-col bg-white">
-    <DataTable :value="shops" lazy tableStyle="min-width: 50rem">
+    <DataTable
+      :value="customers"
+      lazy
+      paginator
+      :first="first"
+      :rows="10"
+      :rowsPerPageOptions="[10, 20, 50]"
+      :totalRecords="totalRecords"
+      :loading="loading"
+      dataKey="id"
+      tableStyle="min-width: 50rem"
+    >
       <Column
         v-for="col of columns"
         :key="col.field"
@@ -21,13 +32,40 @@ definePageMeta({
   layout: 'admin',
 });
 
-const { data: shops, pending } = useLazyFetch('/api/shop/list');
+onMounted(async () => {
+  loading.value = true;
 
-const columns = [
+  lazyParams.value = {
+    first: 0,
+    rows: 10,
+    sortField: null,
+    sortOrder: null,
+  };
+
+  await loadLazyData();
+});
+
+const columns = ref([
   { field: 'id', header: 'ID' },
   { field: 'title', header: 'Название' },
   { field: 'rating', header: 'Рейтинг' },
-];
+]);
+
+const loading = ref(false);
+const lazyParams = ref({});
+const first = ref(0);
+const totalRecords = ref(0);
+const customers = ref();
+
+const loadLazyData = async (event?: any) => {
+  lazyParams.value = { ...lazyParams.value, first: event?.first || first.value };
+
+  const { data, pending } = await useFetch('/api/shop/list');
+  loading.value = pending.value;
+  customers.value = data.value?.fetch;
+  console.log(data.value);
+  totalRecords.value = 1;
+};
 </script>
 
 <style scoped></style>
--- server/api/shop/list.ts
+++ server/api/shop/list.ts
@@ -1,7 +1,17 @@
+import { ref } from 'vue';
+
 export default defineEventHandler(async (event) => {
-  return await $fetch(`/shop`, {
-    baseURL: import.meta.env.API_URL,
-    method: 'GET',
-    headers: event.headers,
-  });
+  const headers = ref();
+  return {
+    fetch: await $fetch(`/shop`, {
+      baseURL: import.meta.env.API_URL,
+      method: 'GET',
+      headers: event.headers,
+      onResponse(context): Promise<void> | void {
+        headers.value = context.response.headers;
+        console.log(context.response.headers);
+      },
+    }),
+    headers: headers,
+  };
 });
